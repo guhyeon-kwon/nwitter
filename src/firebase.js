826edb84eda4ee49c10e79a1dc1876e8eef36b59
GuHyeon <54255662+guhyeon-kwon@users.noreply.github.com>
2022-03-21 01:11:55 +0900

    commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAPOwVXKun8RU_a3GBP8FbIWu4TGBYMiPE",
+    authDomain: "nwitter-b72f4.firebaseapp.com",
+    projectId: "nwitter-b72f4",
+    storageBucket: "nwitter-b72f4.appspot.com",
+    messagingSenderId: "1024263338448",
+    appId: "1:1024263338448:web:2f70dc5a758cb9949e21d5"
+};
+
+export default firebase.initializeApp(firebaseConfig);
