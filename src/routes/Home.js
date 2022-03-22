1f89bf4df686355e99ce19d910ce3e542eea57e1
GuHyeon <54255662+guhyeon-kwon@users.noreply.github.com>
2022-03-22 17:21:34 +0900

    commit

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,28 +1,64 @@
-import React, {useState} from "react";
-import { addDoc, collection } from "firebase/firestore";
+import React, {useEffect, useState} from "react";
+import {addDoc, collection, onSnapshot, orderBy, query} from "firebase/firestore";
 import {dbService} from "fbase";
 
-const Home = () => {
-    const [nweet, setNweet] = useState();
+const Home = ({userObj}) => {
+    console.log(userObj);
+    const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    useEffect(() => {
+        const q = query(
+            collection(dbService, "nweets"),
+            orderBy("createdAt", "desc")
+        );
+        onSnapshot(q, (snapshot) => {
+            const nweetArr = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+            setNweets(nweetArr);
+        });
+    }, []);
     const onSubmit = async (event) => {
         event.preventDefault();
-        const docRef = await addDoc(collection(dbService, "nweets"), {
-            nweet,
-            createdAt: Date.now(),
-        });
+        try {
+            const docRef = await addDoc(collection(dbService, "nweets"), {
+                nweet,
+                createdAt: Date.now(),
+                creatorId: userObj.uid,
+            });
+            console.log("Document written with ID: ", docRef.id);
+        } catch (error) {
+            console.error("Error adding document: ", error);
+        }
         setNweet("");
-    }
+    };
     const onChange = (event) => {
-        const {target:{value}} = event;
+        const {
+            target: { value },
+        } = event;
         setNweet(value);
-    }
+    };
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input value={nweet || ''} onChange={onChange} type="text" placeholder="what on your mind?" maxLength={120}/>
-                <input type="submit" value="Nweet"/>
+                <input
+                    value={nweet}
+                    onChange={onChange}
+                    type="text"
+                    placeholder="What's on your mind?"
+                    maxLength={120}
+                />
+                <input type="submit" value="Nweet" />
             </form>
+            <div>
+                {nweets.map((nweet) => (
+                    <div key={nweet.id}>
+                        <h4>{nweet.nweet}</h4>
+                    </div>
+                ))}
+            </div>
         </div>
-    )
-}
+    );
+};
 export default Home;
